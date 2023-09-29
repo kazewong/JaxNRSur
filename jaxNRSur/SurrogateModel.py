8fbee4e939f30c396583cc263e5035c981dbcf80
Kaze Wong <kazewong.physics@gmail.com>
2023-09-29 15:34:53 -0400

    update surrogate model to get mode

--- jaxNRSur/SurrogateModel.py
+++ jaxNRSur/SurrogateModel.py
@@ -1,23 +1,8 @@
-import h5py
+import jax
 import jax.numpy as jnp
-from jaxNRSur.EIMPredictor import EIMpredictor
 from jaxNRSur.Spline import CubicSpline
+from jaxNRSur.DataLoader import SurrogateDataLoader
 from jaxtyping import Array, Float
-from read_node_info import read_node_fit_info_from_h5
-
-h5_mode_tuple = {
-    "(3,2)": "ITEM_0",
-    "(3,1)": "ITEM_4",
-    "(5,5)": "ITEM_10",
-    "(3,3)": "ITEM_2",
-    "(3,0)": "ITEM_3",
-    "(4,4)": "ITEM_1",
-    "(2,1)": "ITEM_5",
-    "(2,0)": "ITEM_6",
-    "(4,3)": "ITEM_7",
-    "(2,2)": "ITEM_8",
-    "(4,2)": "ITEM_9",
-}
 
 
 def get_T3_phase(q: float, t: Float[Array, str("n")], t_ref: float = 1000.0) -> float:
@@ -38,65 +23,67 @@ def effective_spin(q: float, chi1: float, chi2: float) -> tuple[float, float]:
 
 
 class SurrogateModel:
-    def __init__(self, data_path):
-        data = h5py.File(data_path, "r")
-        self.sur_time = jnp.array(data["domain"])
+    def __init__(
+        self,
+        data_path: str,
+        modelist: list[tuple[int, int]] = [
+            (2, 2),
+            (2, 1),
+            (2, 0),
+            (3, 0),
+            (3, 1),
+            (3, 2),
+            (3, 3),
+            (4, 2),
+            (4, 3),
+            (4, 4),
+            (5, 5),
+        ],
+    ):
+        self.data = SurrogateDataLoader(data_path, modelist=modelist)
 
-        self.amp_data = data["sur_subs/ITEM_8/func_subs/ITEM_1/"]
-        self.phase_data = data["sur_subs/ITEM_8/func_subs/ITEM_0/"]
-
-        assert isinstance(self.amp_data, h5py.Group), "amp_data is not a group"
-        assert isinstance(self.phase_data, h5py.Group), "phase_data is not a group"
-
-        self.amp_B_matrix = jnp.array(self.amp_data["ei_basis"])
-        self.amp_gpr_predictors = self.initialize_EIM_predictor(self.amp_data)
-        self.n_amp_nodes = len(self.amp_gpr_predictors)
-
-        self.phase_B_matrix = jnp.array(self.phase_data["ei_basis"])
-        self.phase_gpr_predictors = self.initialize_EIM_predictor(self.phase_data)
-        self.n_phase_nodes = len(self.phase_gpr_predictors)
-
-    def initialize_EIM_predictor(self, node_data):
-        try:
-            n_nodes = node_data["n_nodes"][()]
-        except ValueError:
-            raise ValueError("n_nodes data doesn't exist")
-
-        predictors = []
-        for count in range(n_nodes):
-            try:
-                h5_GPR_datadump = node_data[
-                    "node_functions/ITEM_%d/node_function/DICT_fit_data" % (count)
-                ]
-            except ValueError:
-                raise ValueError("GPR Fit info doesn't exist")
-            res = read_node_fit_info_from_h5(h5_GPR_datadump)
-            node_predictor = EIMpredictor(res)
-            predictors.append(node_predictor)
-        return predictors
-
+    @staticmethod
     def get_eim(
-        self, params: Float[Array, str("n_dim")]
-    ) -> tuple[Float[Array, str("n_sample")], Float[Array, str("n_sample")]]:
-        amp_result = jnp.zeros(self.n_amp_nodes)
-        phase_result = jnp.zeros(self.n_phase_nodes)
-        for i in range(self.n_amp_nodes):
-            amp_result = amp_result.at[i].set(self.amp_gpr_predictors[i](params)[0])
-        for i in range(self.n_phase_nodes):
-            phase_result = phase_result.at[i].set(
-                self.phase_gpr_predictors[i](params)[0]
-            )
-        return amp_result, phase_result
-
-    def get_waveform(
-        self, time: Float[Array, str("n_sample")], params: dict
+        eim_dict: dict, params: Float[Array, str("n_dim")]
     ) -> Float[Array, str("n_sample")]:
-        chi_hat, chi_a = effective_spin(params[0], params[1], params[2])
-        trans_params = jnp.array([[jnp.log(params[0]), chi_hat, chi_a]])
-        amp_result, phase_result = self.get_eim(trans_params)
-        amp = jnp.dot(self.amp_B_matrix.T, amp_result)
-        phase = -jnp.dot(self.phase_B_matrix.T, phase_result)
-        phase = phase + get_T3_phase(params[0], self.sur_time)
-        interp_amp = CubicSpline(self.sur_time, amp)(time)
-        interp_phase = CubicSpline(self.sur_time, phase)(time)
-        return interp_amp * jnp.exp(1j * interp_phase)
+        result = jnp.stack(
+            jax.tree_util.tree_map(lambda f: f(params), eim_dict["predictors"]), axis=0
+        )
+        return jnp.dot(eim_dict["eim_basis"].T, result)
+
+    def get_mode(
+        self,
+        time: Float[Array, str("n_samples")],
+        params: Float[Array, str("n_dim")],
+        mode: tuple[int, int],
+        orbital_phase: float = 0.0,
+    ) -> Float[Array, str("n_sample")]:
+        if mode not in list(self.data.modes):
+            raise ValueError(f"Mode {mode} not in modelist")
+        elif mode == (2, 2):
+            q = params[0]
+            assert isinstance(q, float)
+            amp = self.get_eim(self.data.modes[mode]["amp"], params)
+            phase = -self.get_eim(self.data.modes[mode]["phase"], params)
+            phase = phase + get_T3_phase(q, self.data.sur_time)
+            amp_interp = CubicSpline(self.data.sur_time, amp)(time)
+            phase_interp = CubicSpline(self.data.sur_time, phase)(time)
+            return amp_interp * jnp.exp(1j * phase_interp)
+        elif mode == (2, 0):
+            real = self.get_eim(self.data.modes[mode]["real"], params)
+            return CubicSpline(self.data.sur_time, real)(time)
+        elif mode == (3, 0):
+            imag = self.get_eim(self.data.modes[mode]["imag"], params)
+            return CubicSpline(self.data.sur_time, imag)(time)
+        else:
+            real = self.get_eim(self.data.modes[mode]["real"], params)
+            imag = self.get_eim(self.data.modes[mode]["imag"], params)
+            return (
+                CubicSpline(self.data.sur_time, real)(time)
+                + 1j * CubicSpline(self.data.sur_time, imag)(time)
+            ) * jnp.exp(1j * mode[1] * orbital_phase)
+
+    # def get_waveform(
+    #     self, time: Float[Array, str("n_sample")], params: dict
+    # ) -> Float[Array, str("n_sample")]:
+    #     pass
