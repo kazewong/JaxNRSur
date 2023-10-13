07f9bbadc7334d9584f82f55211f464089cc3bca
Ethan Payne <epayne@workergpu036.cm.cluster>
2023-10-13 11:59:04 -0400

    started work on NRSur7dq4 data loader

--- jaxNRSur/DataLoader.py
+++ jaxNRSur/DataLoader.py
@@ -127,3 +127,80 @@ class NRHybSur3dq8DataLoader(eqx.Module):
                     )
         result["mode"] = mode
         return result
+
+
+class NRHybSur7dq4DataLoader(eqx.Module):
+    sur_time: Float[Array, str("n_sample")]
+    modes: list[dict]
+
+    def __init__(
+        self,
+        path: str,
+        modelist: list[tuple[int, int]] = [
+            (2, 0),
+            (2, 1),
+            (2, 2),
+            (3, 0),
+            (3, 1),
+            (3, 2),
+            (3, 3),
+            (4, 0),
+            (4, 1),
+            (4, 2),
+            (4, 3),
+            (4, 4),
+        ],
+    ) -> None:
+        data = h5py.File(path, "r")
+        self.t_coorb = jnp.array(data["t_coorb"])
+
+        self.modes_plus = []
+        self.modes_minus = []
+        for i in range(len(modelist)):
+            self.modes_plus.append(self.read_single_mode(data, modelist[i]))
+            self.modes_minus.append(self.read_single_mode(data, modelist[i]))
+
+    def read_function(self, node_data: h5py.Group) -> dict:
+        result = {}
+        n_nodes = int(node_data["n_nodes"][()])  # type: ignore
+        result["n_nodes"] = n_nodes
+
+        predictors = []
+        for count in range(n_nodes):
+            try:
+                fit_data = node_data[
+                    "node_functions/ITEM_%d/node_function/DICT_fit_data"
+                    % (count)
+                ]
+            except ValueError:
+                raise ValueError("GPR Fit info doesn't exist")
+
+            assert isinstance(
+                fit_data, h5py.Group
+            ), "GPR Fit info is not a group"
+            res = h5Group_to_dict(fit_data)
+            node_predictor = EIMpredictor(res)
+            predictors.append(node_predictor)
+
+        result["predictors"] = predictors
+        result["eim_basis"] = jnp.array(node_data["ei_basis"])
+        result["name"] = node_data["name"][()].decode("utf-8")  # type: ignore
+        return result
+            
+
+    def read_single_mode(self, file: h5py.File, mode: tuple[int, int]) -> dict:
+        result = {}
+        if mode[1] != 0:
+            result['real_plus'] = self.read_function(file[f'hCoorb_{mode[0]}_{mode[1]}_Re+'])
+            result['imag_plus'] = self.read_function(file[f'hCoorb_{mode[0]}_{mode[1]}_Im+'])
+            
+            result['real_minus'] = self.read_function(file[f'hCoorb_{mode[0]}_{mode[1]}_Re-'])
+            result['imag_minus'] = self.read_function(file[f'hCoorb_{mode[0]}_{mode[1]}_Im-'])
+            
+        else:
+            result['real'] = self.read_function(file[f'hCoorb_{mode[0]}_{mode[1]}_real'])
+            result['imag'] = self.read_function(file[f'hCoorb_{mode[0]}_{mode[1]}_imag'])
+            
+        result['mode'] = mode
+        
+        return result
