Fixing the waveform rotation to the inertial frame

src/jaxNRSur/SurrogateModel.py
@@ -442,7 +442,7 @@ class NRSur7dq4Model(eqx.Module):
         # but it used to be zero
         # NOTE: Ethan (10/6/24), changing this code following: 
         # https://github.com/sxs-collaboration/gwsurrogate/blob/55dfadb9e62de0f1ae0c9d69c72e49b00a760d85/gwsurrogate/new/precessing_surrogate.py#L714
-        h_lm_plus = jnp.conj(h_lm_sum - h_lm_diff) #(h_lm_sum + jnp.conj(h_lm_diff)) / 2
+        h_lm_plus = jnp.conj(h_lm_sum - h_lm_diff)*(jnp.abs(h_lm_diff) > 1e-12) + h_lm_sum*(jnp.abs(h_lm_diff) < 1e-12) #(h_lm_sum + jnp.conj(h_lm_diff)) / 2
         h_lm_minus = (h_lm_sum + h_lm_diff)*(jnp.abs(h_lm_diff) > 1e-12) #(h_lm_sum - jnp.conj(h_lm_diff)) / 2
 
         return h_lm_plus, h_lm_minus
@@ -496,8 +496,8 @@ class NRSur7dq4Model(eqx.Module):
         matrix_coefs = jnp.zeros((quat_inv.shape[0],  self.n_modes_extended), dtype=complex)
         
         # Handling the if statements, additionally using. a Dirac delta to ensure the ells match
-        ell, m = mode
-        for (ell_p, m_p), i in self.modelist_dict_extended.items():
+        ell_p, m_p = mode
+        for (ell, m), i in self.modelist_dict_extended.items():
 
             matrix_coefs = matrix_coefs.at[i2, i].set(float(ell_p == ell) * float(m_p == -m) * R_B[i2] ** (2 * m) * (-1) ** (ell + m - 1))
             matrix_coefs = matrix_coefs.at[i3, i].set(float(ell_p == ell) * float(m_p == m) * R_A[i3] ** (2 * m))